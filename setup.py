8f96d8f72face520e4d867dd3a02e03458d5edc9
Louis Massingham <louisjohnmassingham@gmail.com>
2025-01-28 14:07:54 +0000

    I added a virtual environment just to learn that process but not sure I will use it for this very small project. I pip installed required packages from a requirements text file. I downloaded and added latest version of python to my path. Lots has been learnt so far just for setup. File structure will not be perfect for this becasue this is my first project but I am trying to research what to include.

---
+++ setup.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf # I need to download TensorFlow next
+from tensorflow import keras
+import seaborn as sns
+import os
+from datetime import datetime
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+
+
