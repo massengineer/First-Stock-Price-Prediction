3a0e2df2d51467a7331da3f47fa43bb65130309b
Louis Massingham <louisjohnmassingham@gmail.com>
2025-01-28 14:20:03 +0000

    TO-DO activities comments added in the setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import tensorflow as tf # I need to download TensorFlow next
+import tensorflow as tf # TO-DO: I need to download TensorFlow next and use pip install for all of these packages in the virtual environment
 from tensorflow import keras
 import seaborn as sns
 import os
